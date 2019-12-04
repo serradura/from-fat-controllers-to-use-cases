ed9cceba15785b81b9a05fe1351c90895d34d1b0
Rodrigo Serradura <rodrigo.serradura@gmail.com>
2019-12-04 01:49:17 -0300

    [v4.1] Service objects with inheritance

--- app/services/todo/finder.rb
+++ app/services/todo/finder.rb
@@ -1,17 +1,14 @@
-class TodoFinder
-  attr_reader :user, :params
-
-  def initialize(user, params)
-    @user = user
-    @params = params
-  end
-
+class Todo::Finder < Todo::ServiceObject
   def find_todo
     user.todos.find(params[:id])
   end
 
-  def find_todos
-    todos.where(user_id: user.id)
+  def find_todos(result_as_json: false)
+    relation = todos.where(user_id: user.id)
+
+    return relation unless result_as_json
+
+    relation.map { |todo| serialize_todo(todo) }
   end
 
   private
