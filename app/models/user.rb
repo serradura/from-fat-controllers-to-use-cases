55769be6dae4f2e4d312a8973622ad86babc54e3
Rodrigo Serradura <rodrigo.serradura@gmail.com>
2019-12-04 00:05:17 -0300

    [v3] Concerns

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-  has_many :todos
+  include Registrable
 
-  attr_accessor :password, :password_confirmation
+  has_many :todos
 
   before_validation :normalize_and_fill_attributes
 
@@ -11,35 +11,11 @@ class User < ApplicationRecord
     validates :password_digest, length: { is: 64 }, if: ->(user) { user.password_digest.present? }
   end
 
-  validate :password_must_be_filled, on: :create
-  validate :password_must_be_confirmed
-
   private
 
     def normalize_and_fill_attributes
       self.name.try(:squish!)
-      self.password.try(:strip!)
-      self.password_confirmation.try(:strip!)
 
       self.token = SecureRandom.uuid if token.blank?
-
-      if password_filled? && password == password_confirmation
-        self.password_digest = Digest::SHA256.hexdigest(password)
-      end
-    end
-
-    def password_must_be_filled
-      errors.add(:password, "can't be blank") if password.blank?
-      errors.add(:password_confirmation, "can't be blank") if password_confirmation.blank?
-    end
-
-    def password_must_be_confirmed
-      if password_filled? && password != password_confirmation
-        errors.add(:password_confirmation, "doesn't match password")
-      end
-    end
-
-    def password_filled?
-      password.present? && password_confirmation.present?
     end
 end
